1717209fd09432e8f43bee6b43b175d84c557686
Overtorment <overtorment@gmail.com>
2020-08-16 15:30:36 +0100

    FIX: explicitly specify wallet file for bitcoind

--- run_scripts/run-bitcoind-full.sh
+++ run_scripts/run-bitcoind-full.sh
@@ -1,2 +1,2 @@
 cd "$(dirname "$0")"
-./bin/bitcoind  -datadir=./datadir  -rpcuser=uuuu  -rpcpassword=pppp -rpcbind=0.0.0.0  -txindex  --zmqpubrawblock=tcp://111.222.333.444:28332   --zmqpubrawtx=tcp://111.222.333.444:28333 -rpcallowip=0.0.0.0/0 -server=1 -deprecatedrpc=accounts
+./bin/bitcoind  -datadir=./datadir  -rpcuser=uuuu  -rpcpassword=pppp -rpcbind=0.0.0.0  -txindex  --zmqpubrawblock=tcp://111.222.333.444:28332   --zmqpubrawtx=tcp://111.222.333.444:28333 -rpcallowip=0.0.0.0/0 -server=1 -deprecatedrpc=accounts -wallet=wallet.dat
--- run_scripts/run-bitcoind.sh
+++ run_scripts/run-bitcoind.sh
@@ -1,2 +1,2 @@
 cd "$(dirname "$0")"
-./bitcoin-0.19.1/bin/bitcoind -txindex=1 -datadir=./bitcoin-0.19.1/datadir  -rpcuser=uuuu  -rpcpassword=pppp -rpcbind=0.0.0.0  -rpcallowip=0.0.0.0/0 -server=1
+./bitcoin-0.19.1/bin/bitcoind -txindex=1 -datadir=./bitcoin-0.19.1/datadir  -rpcuser=uuuu  -rpcpassword=pppp -rpcbind=0.0.0.0  -rpcallowip=0.0.0.0/0 -server=1 -wallet=wallet.dat
