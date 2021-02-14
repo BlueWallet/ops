acbbfcde0a044f1a0fdde0bf39096f2f51084d6b
Overtorment <overtorment@gmail.com>
2021-02-14 14:33:47 +0000

    Update run-lnd.sh

--- run_scripts/run-lnd.sh
+++ run_scripts/run-lnd.sh
@@ -1,4 +1,4 @@
-/home/user/gocode/bin/lnd  --bitcoin.active --bitcoin.mainnet --debuglevel=info --bitcoin.node=bitcoind --bitcoind.rpcuser=uuuu --bitcoind.rpcpass=pppp --bitcoind.zmqpubrawblock=tcp://199.217.117.27:28332 --bitcoind.zmqpubrawtx=tcp://199.217.117.27:28333 --rpclisten=0.0.0.0 --restlisten=0.0.0.0:8080 --bitcoin.defaultchanconfs=1 --bitcoin.defaultremotedelay=6  --lnddir=/home/user/.lnd --datadir=/home/user/.lnd/data
+/home/user/gocode/bin/lnd  --bitcoin.active --bitcoin.mainnet --debuglevel=info --bitcoin.node=bitcoind --bitcoind.rpcuser=uuuu --bitcoind.rpcpass=pppp --bitcoind.zmqpubrawblock=tcp://199.217.117.27:28332 --bitcoind.zmqpubrawtx=tcp://199.217.117.27:28333 --rpclisten=0.0.0.0 --restlisten=127.0.0.1:8080 --bitcoin.defaultchanconfs=1 --bitcoin.defaultremotedelay=6  --lnddir=/home/user/.lnd --datadir=/home/user/.lnd/data --protocol.wumbo-channels --sync-freelist  --neutrino.feeurl=https://nodes.lightning.computer/fees/v1/btc-fee-estimates.json --enable-upfront-shutdown
 
 #--externalip=111.222.333.444
 
