d747a0d4b7d2aef29bc90b61108c672b87f7f4f0
Overtorment <overtorment@gmail.com>
2025-02-14 15:33:17 +0000

    Update run-electrum.sh

--- run_scripts/run-electrum.sh
+++ run_scripts/run-electrum.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 # sudo iptables -t nat -A PREROUTING -p tcp --dport 443 -j DNAT --to-destination 127.0.0.1:50002
-export COIN=BitcoinSegwit 
+ulimit -n 99000
+export COIN=BitcoinSegwit
 export DAEMON_URL=uuuu:pppp@127.0.0.1
 export ALLOW_ROOT=1
 export MAX_SESSIONS=100000
@@ -8,14 +9,13 @@ export MAX_SEND=5000000
 export COST_SOFT_LIMIT=0
 export COST_HARD_LIMIT=0
 export REQUEST_SLEEP=100
-export SSL_CERTFILE=/home/user/server.crt
-export SSL_KEYFILE=/home/user/server.key
-export DB_DIRECTORY=/home/user/electrum_datadir/
-export SERVICES="tcp://:50001,SSL://:50002,wss://:50004"
+export SSL_CERTFILE=/root/.local/share/caddy/certificates/acme-v02.api.letsencrypt.org-directory/electrum1.bluewallet.io/electrum1.bluewallet.io.crt
+export SSL_KEYFILE=/root/.local/share/caddy/certificates/acme-v02.api.letsencrypt.org-directory/electrum1.bluewallet.io/electrum1.bluewallet.io.key
+export DB_DIRECTORY=/mnt/volume_fra1_02/electrum_datadir/
+export SERVICES="tcp://:50001,SSL://:443,wss://:50004"
 
 export INITIAL_CONCURRENT=50000
-export REQUEST_TIMEOUT=15
-export SESSION_TIMEOUT=15
+export REQUEST_TIMEOUT=9
+export SESSION_TIMEOUT=9
 
-electrumx_server 
-
+python3 ./electrumx/electrumx_server
